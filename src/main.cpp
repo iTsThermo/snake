9634c4d0f6ab47f0d0549821cbf34fca3e3004dd
iTsThermo <117327847+iTsThermo@users.noreply.github.com>
2025-07-20 16:32:13 -0400

    switched buttons

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,40 @@
 void menu(){
     sf::RenderWindow window(sf::VideoMode({1080,720}), "Main Menu");
 
+    sf::Color backgroundColor = sf::Color::Green;
+
+
     sf::Font ariel;
     ariel.loadFromFile("../ARIAL.TTF");
+
+    sf::Text snake_menu_text;
+    snake_menu_text.setFont(ariel);
+    snake_menu_text.setString("SNAKE GAME");
+    snake_menu_text.setFillColor(sf::Color::Black);
     
     sf::Text start_text;
     start_text.setFont(ariel);
     start_text.setString("Start");
-    start_text.setFillColor(sf::Color::Red);
+    start_text.setFillColor(sf::Color::Black);
 
     sf::Text exit_text;
     exit_text.setFont(ariel);
     exit_text.setString("Exit");
-    exit_text.setFillColor(sf::Color::Red);
+    exit_text.setFillColor(sf::Color::Black);
+
     sf::RectangleShape start_button({100, 50});
     sf::RectangleShape exit_button({100, 50});
-
 
     sf::Vector2u window_size = window.getSize();
     std::cout << std::endl <<"X:" << window_size.x << "Y:" << window_size.y;
 
-    start_button.setPosition(window_size.x / 2, window_size.y / 2);
-    start_text.setPosition(window_size.x / 2, window_size.y / 2);
+    start_text.setPosition(window_size.x / 2.75, window_size.y / 2);
+    start_button.setPosition(window_size.x / 2.75, window_size.y / 2);
+    
+    exit_text.setPosition(window_size.x / 1.75, window_size.y / 2);
+    exit_button.setPosition(window_size.x / 1.75, window_size.y / 2);
 
-    exit_button.setPosition(window_size.x / 2.5, window_size.y / 2);
-    exit_text.setPosition(window_size.x / 2.5, window_size.y / 2);
+    snake_menu_text.setPosition(window_size.x / 2.25, window_size.y / 3);
 
     while (window.isOpen())
     {
@@ -39,12 +49,14 @@ void menu(){
                 window.close();
         }
 
-        window.clear();
+        window.clear(backgroundColor);
+
+        window.draw(snake_menu_text);
 
         window.draw(exit_button);
-        window.draw(exit_text);
-
         window.draw(start_button);
+        
+        window.draw(exit_text);
         window.draw(start_text);
 
 
